acdd63518a8f3fae52bf1316f52e6bea8298bd5e
askingtheology <167041062+askingtheology@users.noreply.github.com>
2024-04-22 19:26:07 +1000

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,3 @@
+// You can add JavaScript code here if needed.
+// However, for a static website like this, you might not need any JavaScript functionality.
+// This file is included for potential future enhancements or interactivity.
